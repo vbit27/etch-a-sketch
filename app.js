6d6acf80385e8216053d939e0d35139765cb0cee
Vasil Bit <vasil.bituni@gmail.com>
2021-02-18 14:56:03 +0100

    Add changes to the grid

--- app.js
+++ app.js
@@ -1,34 +1,28 @@
-const sketchContainer = document.getElementById('container');
-
-const sketchField = document.createElement('div');
 
 
+const sketchContainer = document.getElementById('sketch-container');
 
 
 
 
 function addBoxes(num) {
-    sketchContainer.style.gridTemplateColumns = `repeat(${num}, auto)`
-    sketchContainer.style.gridTemplateRows = `repeat(${num}, auto)`
+
+    //repeat(auto-fit, minmax(75px, 1fr))
+    sketchContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+
     
     for (let i = 0; i < num*num; i++) {
-        
-
         const sketchField = document.createElement('div');
         sketchField.style.backgroundColor = 'white';
-        sketchField.style.padding = '2em';
-        sketchField.style.maxWidth = '100%';
-        sketchField.style.maxHeight = '100%';
-
-        sketchField.style.border = '0.1px solid grey';
+        sketchContainer.style.height = '0';
+        sketchField.style.paddingBottom = '70%';
+        sketchField.style.border = '1px solid grey';
         sketchField.classList = `box${i}`;
-
-        
         sketchContainer.appendChild(sketchField);
 
     }
 }
 
-addBoxes(15)
+addBoxes(50)
 
 
