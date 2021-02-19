b4302720b19c8e0c8c65eaf93ec979130c829b06
Vasil Bit <vasil.bituni@gmail.com>
2021-02-19 13:25:03 +0100

    Add fix the button for changing the size

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 const sketchContainer = document.getElementById('sketch-container');
+const pickSizeButton = document.getElementById('pick-size');
+
+let size;
+let sketchSquares;
 
 
-// Add boxes inside container
+function pickSize() {
+    size = prompt('Pick a size between 16 and 50.');
+        if (size < 16 || size > 50 || size == NaN) {
+            alert('Only type a number between 16 and 50.');
+            return size = 30;
+        }  
+    sketchContainer.querySelectorAll('*').forEach(n => n.remove()); //delets the old divs
+    addBoxes(size);
+}
+
+
+// Add boxes inside container & initialize painting
 
 function addBoxes(size = 20) {
     sketchContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-
-    for (let i = 0; i < size*size; i++) {
-        const sketchField = document.createElement('div');
-        sketchField.classList.add('boxes');
-        sketchField.accessKey = `${i}`;
-        sketchContainer.appendChild(sketchField);
-    }
+        for (let i = 0; i < size*size; i++) {
+            const sketchField = document.createElement('div');
+            sketchField.classList.add('boxes');
+            sketchField.accessKey = `${i}`;
+            sketchContainer.appendChild(sketchField);
+        }
+    sketchSquares = document.querySelectorAll('.boxes');
+    startPainting(); //initialize painting
 }
-
-let size = 20;
-
-
-addBoxes(50)
 
 // Change color when mouse over
-const sketchSquares = document.querySelectorAll('.boxes');
 
-
-sketchContainer.addEventListener('mouseover', function(e) {
+function startPainting() {
+    sketchContainer.addEventListener('mouseover', function(e) {
     if (e.target !== e.currentTarget) {
         sketchSquares.item(e.target.accessKey).classList.add('black');  
-    }
-})
-
-
-const pickSizeButton = document.getElementById('pick-size');
-
-pickSizeButton.addEventListener('click', pickSize)
-
-function pickSize() {
-    size = prompt('Pick a size between 16 and 50.')
-        if (size < 16 || size > 50 || size == NaN) {
-            alert('Only type a number between 16 and 50.');
-            return size = 30;
-        } else 
-        sketchContainer.querySelectorAll('*').forEach(n => n.remove());
-        return  addBoxes(size);
+        }
+    })
 }
+
+
+pickSizeButton.addEventListener('click', pickSize);
+addBoxes();
 
 
