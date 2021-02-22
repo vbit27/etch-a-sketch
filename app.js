d509ec49f7fac23b8ff00e42c83ef09991287bb1
Vasil Bit <vasil.bituni@gmail.com>
2021-02-22 08:50:45 +0100

    Add random color option

--- app.js
+++ app.js
@@ -8,74 +8,68 @@ let size;
 let sketchSquares;
 
 
-function pickSize() {
-    size = prompt('Pick a size between 16 and 50.');
-        if (size < 16 || size > 50 || size == NaN) {
-            alert('Only type a number between 16 and 50.');
-            return size = 30;
-        }  
-    sketchContainer.querySelectorAll('*').forEach(n => n.remove()); //delets the old divs
-    addBoxes(size);
-}
-
-
 // Add boxes inside container & initialize painting
 
-function addBoxes(size = 20) {
+const addBoxes = (size = 20) => {
     sketchContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    sketchContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
         for (let i = 0; i < size*size; i++) {
             const sketchField = document.createElement('div');
             sketchField.classList.add('boxes');
             sketchField.accessKey = `${i}`;
             sketchContainer.appendChild(sketchField);
         }
-    sketchSquares = document.querySelectorAll('.boxes');
+    sketchSquares = sketchContainer.querySelectorAll('.boxes');
     startPainting(); //initialize painting
-}
+};
 
 // Change color when mouse over (Black)
 
-function startPainting() {
-    sketchContainer.addEventListener('mouseover', function(e) {
-    if (e.target !== e.currentTarget) {
-        sketchSquares.item(e.target.accessKey).classList.add('black'); 
-        sketchSquares.item(e.target.accessKey).classList.remove('red'); 
-
-        }
-    })
-}
+const startPainting = () => {
+        sketchContainer.addEventListener('mouseover', (e) => {
+        if (e.target !== e.currentTarget) {
+            sketchSquares[e.target.accessKey].style.backgroundColor = 'grey';
+        }})
+};
 
 // Change color when mouse over (Red)
 
-function startPaintingRed() {
+const startPaintingRed = () => {
     sketchContainer.addEventListener('mouseover', function(e) {
         if (e.target !== e.currentTarget) {
-            sketchSquares.item(e.target.accessKey).classList.add('red');
-            }
-        })
-}
+            sketchSquares[e.target.accessKey].style.backgroundColor = 
+            '#' + Math.random().toString(16).substr(-6);
+        }
+    })
+};
 
 
 //Reset all settings
 
-function reset () {
-    sketchContainer.querySelectorAll('*').forEach(n => n.remove()); //delets the old divs
-    addBoxes(size);
-}
+const reset = () => {
+    sketchSquares.forEach(n => n.style.backgroundColor = '');
+};
 
+
+//Change the size of the container
+
+const pickSize = () => {
+    size = prompt('Pick a size between 16 and 60.');
+        if (size < 16 || size > 60 || size == NaN) {
+            alert('Only type a number between 16 and 60.');
+            return size = 30;
+        }  
+    sketchContainer.querySelectorAll('.boxes').forEach(n => n.remove()); //delets the old divs
+    addBoxes(size);
+};
+
+
+addBoxes();
 
 pickSizeButton.addEventListener('click', pickSize);
 resetSettings.addEventListener('click', reset);
 changeColor.addEventListener('click', startPaintingRed);
 changeColorBlack.addEventListener('click', startPainting);
-addBoxes();
 
 
-
-
-
-
-
-
-// PYET NE DISCORD
-//sketchContainer.querySelectorAll('*').forEach(n => n.remove()); //delets the old divs
